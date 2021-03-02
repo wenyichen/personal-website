053e70fca8b7b7c1e8b8d9801159f7ed58a9dd06
Wenyi Chen <wenyichen@outlook.com>
2021-03-01 19:27:27 -0800

    Add font

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css'
 import ReactDOM from 'react-dom';
 import reportWebVitals from './utils/reportWebVitals';
 import { App } from './modules/index/App';
