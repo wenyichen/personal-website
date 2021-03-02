ebd87a7d9ea78b7a68272b94e2095239caf5a810
Wenyi Chen <wenyichen@outlook.com>
2021-03-01 20:27:10 -0800

    Styling

---
+++ src/constants/constants.tsx
@@ -0,0 +1,4 @@
+export const linkedinUrl = "https://www.linkedin.com/in/wenyichen-cs/";
+export const githubUrl = "https://github.com/wenyichen";
+export const emailUrl = "mailto:wenyichen0310@gmail.com";
+export const mediumUrl = "https://medium.com/@wenyichen0310"
--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,12 +1,15 @@
+import { emailUrl, githubUrl, linkedinUrl, mediumUrl } from '../../constants/constants';
 import './home.scss';
+
 
 export const Home = () => (
     <div className="home">
         <div className="home-menu">
-            <a>blog</a>
-            <a>linkedin</a>
-            <a>github</a>
-            <a>resume</a>
+            <a target="_blank" href={mediumUrl}>blog</a>
+            <a target="_blank" href={linkedinUrl}>linkedin</a>
+            <a target="_blank" href={githubUrl}>github</a>
+            <a target="_blank" href={emailUrl}>email</a>
+            <a href="">resume</a>
         </div>
     </div>
 )
