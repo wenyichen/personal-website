c3b7deef1781b52cc559dea4219f2746a7cbaf66
Wenyi Chen <wenyichen@outlook.com>
2021-03-01 19:18:23 -0800

    Reorganized files

---
+++ src/modules/index/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Home } from '../home/home';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
