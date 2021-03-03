869ac41d64fcabaccb20880b66fe00b6e50606ae
Wenyi Chen <wenyichen@outlook.com>
2021-03-03 02:57:50 -0800

    Copy email function

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,5 +1,5 @@
 export const linkedinUrl = "https://www.linkedin.com/in/wenyichen-cs/";
 export const githubUrl = "https://github.com/wenyichen";
-export const emailUrl = "mailto:wenyichen0310@gmail.com";
+export const emailAddress = "wenyichen0310@gmail.com";
 export const mediumUrl = "https://medium.com/@wenyichen0310";
 export const resumeFileId = "1cH1u8KBP2EJSPJAuGXQMRtnLQRqvc2r2FasjSD6BxhY";
--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
-  emailUrl,
+  emailAddress,
   githubUrl,
   linkedinUrl,
   mediumUrl,
 } from "../../constants/constants";
 import "./home.scss";
+
+const Email = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
+
+  let displayText = "email";
+  if (isHovered) {
+    displayText = emailAddress;
+  }
+  if (isCopied) {
+    displayText = "copied";
+  }
+
+  const transitions = useTransition(displayText, null, {
+    from: {
+      position: "absolute",
+      left: "-50%",
+      transform: "translateX(-50%)",
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+    },
+    leave: { opacity: 0 },
+  });
+
+  const onCopy = () => {
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 1500);
+  };
+
+  return (
+    <span
+      className="home-menu-email"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {transitions.map(({ item, key, props }) => (
+        <CopyToClipboard text={emailAddress} onCopy={onCopy}>
+          <animated.a key={key} style={props}>
+            {item}
+          </animated.a>
+        </CopyToClipboard>
+      ))}
+    </span>
+  );
+};
 
 export const Home = () => (
   <div className="home">
@@ -21,9 +70,7 @@ export const Home = () => (
         github
       </a>
       <Link to="/resume">resume</Link>
-      <a target="_blank" rel="noreferrer" href={emailUrl}>
-        email
-      </a>
+      <Email />
     </div>
   </div>
 );
