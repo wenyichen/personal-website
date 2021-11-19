b3159d911f81116c8e8336bd43663af57c84bc61
Wenyi Chen <wenyichen@outlook.com>
2021-11-18 22:52:50 -0800

    Change description

--- src/modules/about/about.tsx
+++ src/modules/about/about.tsx
@@ -1,4 +1,9 @@
-import { emailUrl, linkedinUrl } from "../../constants/constants";
+import {
+  emailUrl,
+  linkedinUrl,
+  tinystacksUrl,
+  databrewUrl,
+} from "../../constants/constants";
 import "./about.scss";
 
 export const About = () => (
@@ -7,29 +12,29 @@ export const About = () => (
       <h3 className="about-title">about</h3>
       <p>hi! i'm wenyi.</p>
       <p>
-        i'm a software engineer currently working at aws in the bay area. before
-        that, i studied at ucsd for my undergrad in computer science.
+        i'm a software engineer currently working at{" "}
+        <a target="_blank" rel="noreferrer" href={tinystacksUrl}>
+          tinystacks
+        </a>
+        . before that, i studied at ucsd for my undergrad in computer science
+        then spent two years at aws building{" "}
+        <a target="_blank" rel="noreferrer" href={databrewUrl}>
+          databrew
+        </a>
+        .
       </p>
       <p>
         i'm interested in full-stack web development and artificial
         intelligence. i enjoy delivering new products and tackling hard
-        challenges. 
+        challenges.
       </p>
+      <p>in my free time i like to trade stocks, climb rocks, and travel.</p>
       <p>
-        in my free time i like to trade stonks, drink beer, and travel.
-      </p>
-      <p>
-        feel free to connect on
+        feel free to connect on{" "}
         <a target="_blank" rel="noreferrer" href={linkedinUrl}>
-          {" "}
-          linkedin{" "}
-        </a>
-        or reach out through
-        <a href={emailUrl}>
-          {" "}
-          email
-        </a>
-        !
+          linkedin
+        </a>{" "}
+        or reach out through <a href={emailUrl}>email</a>!
       </p>
     </div>
   </div>
--- src/modules/header/header.tsx
+++ src/modules/header/header.tsx
@@ -5,6 +5,6 @@ import "./header.scss";
 export const Header = () => (
   <div className="header">
     <Link to="/">wenyi chen</Link>
-    <div>software engineer @ aws</div>
+    <div>software engineer</div>
   </div>
 );
